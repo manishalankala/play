7e76e9a3797cae2fcad29c9e6dc762c5eec3b818
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-08-30 14:40:00 +0200

    Update main1.tf

--- tf/main1.tf
+++ tf/main1.tf
@@ -81,3 +81,36 @@ resource "azurerm_app_service" "my_app_service_container" {
    type  = "SQLServer"
    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
 }
+
+
+##################
+## app service - staging slot
+##################
+
+resource "azurerm_app_service_slot" "my_app_service_container_staging" {
+ name                    = "staging"
+ app_service_name        = azurerm_app_service.my_app_service_container.name
+ location                = "west us"
+ resource_group_name     = "RG"
+ app_service_plan_id     = azurerm_app_service_plan.my_service_plan.id
+ https_only              = true
+ client_affinity_enabled = true
+ site_config {
+   scm_type          = "VSTSRM". # or can be "LocalGit"
+   always_on         = "true"
+   health_check_path = "/login"
+ }
+
+ identity {
+   type         = "SystemAssigned, UserAssigned"
+   identity_ids = [data.azurerm_user_assigned_identity.assigned_identity_acr_pull.id]
+ }
+
+ app_settings = local.env_variables 
+}
+
+ connection_string {
+   name  = "Database"
+   type  = "SQLServer"
+   value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+}
